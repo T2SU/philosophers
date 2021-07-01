dfe555763caedf32e9eb01c8ce3ba9b862b6894d
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-07-01 13:41:23 +0900

    working...

--- philo/main.c
+++ philo/main.c
@@ -6,11 +6,42 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 18:04:18 by smun              #+#    #+#             */
-/*   Updated: 2021/06/30 18:05:23 by smun             ###   ########.fr       */
+/*   Updated: 2021/06/30 19:36:17 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "philo.h"
+
 int		main(void)
 {
+	struct timeval time;
+
+	gettimeofday(&time, NULL);
+	suseconds_t first = time.tv_usec;
+	for (int i = 0; i < 1; i++)
+		usleep(1);
+	gettimeofday(&time, NULL);
+	suseconds_t end = time.tv_usec;
+	printf("%u, %u, %u\n", first, end, end - first);
 	return (0);
+}
+
+t_bool	init_forks(t_fork *fork)
+{
+	if (0 != pthread_mutex_init(&fork->mutex, NULL))
+		return (FALSE);
+	fork->state = kNotUsing;
+	return (TRUE);
+}
+
+void	run_philosopher(t_philo *philo)
+{
+
+}
+
+t_bool	init_philosopher(t_philo *philo)
+{
 }
---
+++ philo/philo.h
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/30 18:47:36 by smun              #+#    #+#             */
+/*   Updated: 2021/06/30 19:35:32 by smun             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <sys/time.h>
+# define TRUE 1
+# define FALSE 0
+
+typedef int			t_bool;
+
+enum				e_philo_state
+{
+	kThinking,
+	kEating,
+	kSleeping,
+	kDead
+};
+
+enum				e_fork_state
+{
+	kNotUsing,
+	kUsing
+};
+
+enum				e_dir
+{
+	kLeft,
+	kRight
+};
+
+typedef struct		s_philo
+{
+	pthread_t		thread;
+	pthread_mutex_t	mutex;
+	int				state;
+	suseconds_t		begin_time;
+}					t_philo;
+
+typedef struct		s_fork
+{
+	pthread_mutex_t	mutex;
+	int				state;
+}					t_fork;
+
+typedef struct		s_table
+{
+	pthread_mutex_t	mutex;
+	int				numbers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_to_eat;
+	t_philo			*philosophers;
+	t_fork			*forks;
+}					t_table;
+// 1 f 2 f 3 f 4 f 5
+/*
+** ============================================================================
+**   [[ .c ]]
+** ============================================================================
+*/
+
+/*
+** ============================================================================
+**   [[ time.c ]]
+** ============================================================================
+*/
+
+#endif
