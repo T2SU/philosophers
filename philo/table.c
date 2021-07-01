8b92f3853496aa6259dc8f5d825812fc63bdb4f0
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-07-01 16:06:26 +0900

    aaa

--- philo/table.c
+++ philo/table.c
@@ -6,13 +6,36 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 14:48:30 by smun              #+#    #+#             */
-/*   Updated: 2021/07/01 14:52:11 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/01 15:24:26 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <stdlib.h>
+#include <pthread.h>
+
+t_bool	init_table(int numbers, t_table *table)
+{
+	table->numbers = numbers;
+	table->philosophers = malloc(sizeof(t_philo) * numbers);
+	table->forks = malloc(sizeof(t_fork) * numbers);
+	if (table->philosophers == NULL)
+		return (FALSE);
+	if (table->forks == NULL)
+		return (FALSE);
+}
 
 void	free_table(t_table *table)
 {
-	
+	int	i;
+
+	i = 0;
+	while (i < table->numbers)
+	{
+		pthread_mutex_destroy(&table->forks[i].mutex);
+		pthread_mutex_destroy(&table->philosophers[i].mutex);
+		i++;
+	}
+	free(table->forks);
+	free(table->philosophers);
 }
--- philo/utils.c
+++ philo/utils.c
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 14:06:39 by smun              #+#    #+#             */
-/*   Updated: 2021/07/01 14:09:01 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/01 15:19:34 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	ft_bzero(void *s, size_t n)
 	size_t			*dest_large;
 	size_t			i;
 
-	dest = (unsigned char*)s;
+	dest = (unsigned char *)s;
 	i = n % sizeof(size_t);
 	while (i--)
 		*(dest++) = 0;
-	dest_large = (size_t*)dest;
+	dest_large = (size_t *)dest;
 	i = n / sizeof(size_t);
 	while (i--)
 		*(dest_large++) = 0;
