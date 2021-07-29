27f42b4d5509fde6b8982a7e080e972c8a892807
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-07-29 18:44:38 +0900

    improve performance?

--- philo_bonus/srcs/variation/context_process.c
+++ philo_bonus/srcs/variation/context_process.c
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 20:29:05 by smun              #+#    #+#             */
-/*   Updated: 2021/07/29 18:41:24 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/29 18:43:46 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ static t_bool	context_update(t_context *ctx)
 		if (ctx->info->specified_number_to_eat)
 			if (philo->numbers_had_meal >= ctx->info->number_to_eat)
 				break ;
-		usleep(100);
+		usleep(250);
 	}
 	return (TRUE);
 }
