14f786bbf644046c2ac77b88d29f4049de28237f
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-07-30 02:54:18 +0900

    extract method

--- philo_bonus/srcs/logic/context_died_monitor.c
+++ philo_bonus/srcs/logic/context_died_monitor.c
@@ -6,28 +6,41 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 01:58:25 by smun              #+#    #+#             */
-/*   Updated: 2021/07/30 02:35:52 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/30 02:52:09 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 #include <signal.h>
 
-static void	*died_monitor_run(void *p_sim)
+static void	kill_all_childs(t_simulator *sim)
 {
-	t_simulator	*sim;
-	int			i;
+	int	i;
 
-	sim = (t_simulator *)p_sim;
 	i = 0;
-	monitor_wait(&sim->died_monitor);
 	while (i < sim->info.numbers)
 		kill(sim->contexts[i++].pid, SIGTERM);
+}
+
+static void	unwait_full_monitor(t_simulator *sim)
+{
+	int	i;
+
 	if (!sim->info.specified_number_to_eat)
-		return (NULL);
+		return ;
 	i = -1;
 	while (++i < sim->info.numbers)
 		monitor_notify(&sim->full_monitor);
+}
+
+static void	*died_monitor_run(void *p_sim)
+{
+	t_simulator	*sim;
+
+	sim = (t_simulator *)p_sim;
+	monitor_wait(&sim->died_monitor);
+	kill_all_childs(sim);
+	unwait_full_monitor(sim);
 	return (NULL);
 }
 
--- philo_bonus/srcs/logic/context_full_monitor.c
+++ philo_bonus/srcs/logic/context_full_monitor.c
@@ -6,12 +6,21 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/30 01:59:10 by smun              #+#    #+#             */
-/*   Updated: 2021/07/30 02:34:11 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/30 02:52:57 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 #include <signal.h>
+
+static void	kill_all_childs(t_simulator *sim)
+{
+	int	i;
+
+	i = 0;
+	while (i < sim->info.numbers)
+		kill(sim->contexts[i++].pid, SIGTERM);
+}
 
 static void	*full_monitor_run(void *p_sim)
 {
@@ -22,9 +31,7 @@ static void	*full_monitor_run(void *p_sim)
 	i = -1;
 	while (++i < sim->info.numbers)
 		monitor_wait(&sim->full_monitor);
-	i = 0;
-	while (i < sim->info.numbers)
-		kill(sim->contexts[i++].pid, SIGTERM);
+	kill_all_childs(sim);
 	return (NULL);
 }
 
